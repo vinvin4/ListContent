f27ca486d755fd71e6f97276013f6ea5f54b7e36
vinvin4 <batkovich.vano@mail.ru>
2022-03-14 13:03:59 +0300

    Content View
     - Test project, written on Xamarin.Android.
    
    Core-project contain business-logic implementation - receiving content, pre-processing and providing;
    Androdi project contains UI-part, visualisation content
    CoreTest - Unit tests for Core-project

---
+++ ListContent/Core/SampleClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class SampleClass
+    {
+        public SampleClass()
+        {
+
+        }
+
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
